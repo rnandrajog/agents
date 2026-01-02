e07a35b034e4946ecfe53a4bb9be39c0e49f55fb
Rakesh Nandrajog <r_nandrajog@yahoo.com>
2026-01-02 15:21:08 -0500

    code added to get a question from OpenAI and call it again withthe question to get the answer, mimicing basic Agent AI behaviour

--- OpenAI+/simple_OpenAI_call.py
+++ OpenAI+/simple_OpenAI_call.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
+
 # load the environment variables from the .env file
 load_dotenv(override=True)
 
@@ -9,17 +10,30 @@ load_dotenv(override=True)
 openai_api_key= os.getenv("OPENAI_API_KEY")
 
 if openai_api_key is None or openai_api_key == "":
-     print("OPENAI_API_KEY is not set")
+   #  print("OPENAI_API_KEY is not set")
      raise ValueError("OPENAI_API_KEY is not set")
    
 else:
-    print(openai_api_key)
+    print("key loaded")
 
 # create a new OpenAI client
 openai = OpenAI()
-messages =[{"role":"user", "content":"what is 4+3"}]
+#messages =[{"role":"user", "content":"what is 4+3"}]
+question ="Please propose a hard, challenging question to assess someone's IQ, Respond only with question."
+messages =[{"role":"user", "content":question}]
 response = openai.chat.completions.create(
     model="gpt-4o-mini",
     messages=messages
 )
-print(response.choices[0].message.content)
+#store the question in the question variable
+question =response.choices[0].message.content
+print(question)
+
+messages =[{"role":"user", "content":question}]
+response = openai.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=messages
+)
+answer = response.choices[0].message.content
+print(answer)
+
